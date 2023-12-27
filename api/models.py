6fbc584577bde4df9a2309d5b4910c619df84ca7
20201181110036 <leandro.moura@escolar.ifrn.edu.br>
2023-12-27 11:51:18 -0300

    Consertando erros de ortografia

--- api/models.py
+++ api/models.py
@@ -1,8 +1,7 @@
 from django.db import models
     
 class User(models.Model):
-    nome = models.CharField(max_length=100)
-    userneme = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
     email = models.EmailField()
     address = models.CharField(max_length=20)
     phone = models.CharField(max_length=100)
